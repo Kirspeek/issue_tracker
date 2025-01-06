7030841f1ca99c98f2b46261f2a1b51547865b9a
Kirspeek <kira.cherepenko@gmail.com>
2025-01-06 20:07:08 +0100

    new

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,13 +1,23 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 // Schema for validating the object that is sent with requests
 export const issueSchema = z.object({
-  title: z.string().min(1, 'A title is required.').max(255),
-  description: z.string().min(1, 'A description is required.').max(65535)
-})
+  title: z.string().min(1, "A title is required.").max(255),
+  description: z.string().min(1, "A description is required.").max(65535),
+  status: z.enum(["OPEN", "IN_PROGRESS", "CLOSED"]).optional(),
+});
 
 export const patchIssueSchema = z.object({
-  title: z.string().min(1, 'A title is required.').max(255).optional(),
-  description: z.string().min(1, 'A description is required.').max(65535).optional(),
-  assignedToUserId: z.string().min(1, "AssignedToUserId is required.").max(255).optional().nullable()
-})
+  title: z.string().min(1, "A title is required.").max(255).optional(),
+  description: z
+    .string()
+    .min(1, "A description is required.")
+    .max(65535)
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId is required.")
+    .max(255)
+    .optional()
+    .nullable(),
+});
